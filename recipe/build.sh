ae546c69d11ae430dc87dced3f9b5d8084a6c67d
TS-ANC <208963744+TS-ANC@users.noreply.github.com>
2026-07-13 15:02:09 +0200

    prevent pkgconfig from using host pc files

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -ex
 
+# prevent pkgconfig from looking at host environment .pc files
+export PKG_CONFIG_LIBDIR="${PREFIX}/lib/pkgconfig:${PREFIX}/share/pkgconfig"
+
 ./configure --prefix=$PREFIX --disable-static
 make -j${CPU_COUNT}
 make check
