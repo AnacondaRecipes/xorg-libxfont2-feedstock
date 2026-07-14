e5a5b805f5834f851e6cdd0fddc36ffb2340ffa6
TS-ANC <208963744+TS-ANC@users.noreply.github.com>
2026-07-14 10:00:12 +0200

    xorg-libxfont2 2.0.8  (#3)
    * version 2.0.8
    
    * remove fontconfig
    
    * remove redundant version pinnings
    
    * run upstream tests
    
    * skip static lib
    
    * prevent pkgconfig from using host pc files
    
    * freetype version pinning
    
    * add python build dep for tests

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -ex
 
-./autogen.sh
-./configure --prefix=$PREFIX
-make
+# prevent pkgconfig from looking at host environment .pc files
+export PKG_CONFIG_LIBDIR="${PREFIX}/lib/pkgconfig:${PREFIX}/share/pkgconfig"
+
+./configure --prefix=$PREFIX --disable-static
+make -j${CPU_COUNT}
+make check
 make install
